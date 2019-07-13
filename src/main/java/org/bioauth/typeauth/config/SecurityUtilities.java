7f0992d7295aff5b4f7221b8917980c97937f83c
xbluecode <besbes.af@gmail.com>
2019-07-13 17:50:31 +0300

    Implementing Security Utilities

---
+++ src/main/java/org/bioauth/typeauth/config/SecurityUtilities.java
@@ -0,0 +1,30 @@
+package org.bioauth.typeauth.config;
+
+import org.bioauth.typeauth.domain.Client;
+import org.bioauth.typeauth.service.ClientServiceDb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtilities {
+
+	private ClientServiceDb clientServiceDb;
+
+	@Autowired
+	public SecurityUtilities(ClientServiceDb clientServiceDb) {
+		this.clientServiceDb = clientServiceDb;
+	}
+
+	public Client getAuthenticatedClient()
+	{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!auth.isAuthenticated())
+			return null;
+		Client client = (Client) clientServiceDb.loadClientByClientId(auth.getName());
+		return client;
+	}
+}
