0aa7dbea06134dd03dca57e0244615ea68ad53c0
xbluecode <besbes.af@gmail.com>
2019-07-15 10:54:37 +0300

    Updating PersonController

--- src/main/java/org/bioauth/typeauth/controller/PersonController.java
+++ src/main/java/org/bioauth/typeauth/controller/PersonController.java
@@ -1,6 +1,5 @@
 package org.bioauth.typeauth.controller;
 
-import javassist.NotFoundException;
 import org.bioauth.typeauth.config.ClientSecurityUtil;
 import org.bioauth.typeauth.domain.Client;
 import org.bioauth.typeauth.domain.Person;
@@ -9,13 +8,11 @@ import org.bioauth.typeauth.exception.PersonExistException;
 import org.bioauth.typeauth.exception.PersonNotFoundException;
 import org.bioauth.typeauth.service.ClientServiceDb;
 import org.bioauth.typeauth.service.PersonServiceDb;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 @Controller
@@ -36,17 +33,11 @@ public class PersonController {
 	@GetMapping("/check")
 	public ResponseEntity<?> checkPerson(@RequestParam("name") String name)
 	{
-		Client authenticatedClient;
-		Person person;
+		Person person = getValidPersonByName(name);
 		HashMap<String, Object> body = new HashMap<>();
 
-		authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
-		if (authenticatedClient == null)
-			throw new ClientNotAuthenticatedException("anonymous");
-		if ((person = authenticatedClient.personExist(name)) == null)
-			throw new PersonNotFoundException(name);
-
 		body.put("status", HttpStatus.FOUND);
+		body.put("action", "found");
 		body.put("person", person);
 		return new ResponseEntity<>(body, HttpStatus.CREATED);
 	}
@@ -57,9 +48,7 @@ public class PersonController {
 		Client authenticatedClient;
 		HashMap<String, Object> body = new HashMap<>();
 
-		authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
-		if (authenticatedClient == null)
-			throw new ClientNotAuthenticatedException("anonymous");
+		authenticatedClient = getValidAuthenticatedClient();
 		if (authenticatedClient.personExist(person.getName()) != null)
 			throw new PersonExistException(person.getName());
 		authenticatedClient.getPersons().add(person);
@@ -67,6 +56,7 @@ public class PersonController {
 		person.setId(personServiceDb.findPersonByName(person.getName()).getId());
 
 		body.put("status", HttpStatus.CREATED);
+		body.put("action", "created");
 		body.put("person", person);
 		return new ResponseEntity<>(body, HttpStatus.CREATED);
 	}
@@ -74,21 +64,15 @@ public class PersonController {
 	@PutMapping("/update")
 	public ResponseEntity<?> updatePerson(@RequestBody Person person)
 	{
-		Client authenticatedClient;
-		Person oldPerson;
+		Person oldPerson = getValidPersonByName(person.getName());
 		HashMap<String, Object> body = new HashMap<>();
-
-		authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
-		if (authenticatedClient == null)
-			throw new ClientNotAuthenticatedException("anonymous");
-		if ((oldPerson = authenticatedClient.personExist(person.getName())) == null)
-			throw  new PersonNotFoundException(person.getName());
 
 		person.setId(oldPerson.getId());
 		oldPerson = oldPerson.copy();
 		personServiceDb.update(person);
 
 		body.put("status", HttpStatus.OK);
+		body.put("action", "updated");
 		body.put("old", oldPerson);
 		body.put("new", person);
 		return new ResponseEntity<>(body, HttpStatus.OK);
@@ -97,15 +81,9 @@ public class PersonController {
 	@DeleteMapping("/delete")
 	public ResponseEntity<?> deletePerson(@RequestParam("name") String name)
 	{
-		Client authenticatedClient;
-		Person person;
+		Person person = getValidPersonByName(name);
 		HashMap<String, Object> body = new HashMap<>();
-
-		authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
-		if (authenticatedClient == null)
-			throw new ClientNotAuthenticatedException("anonymous");
-		if ((person = authenticatedClient.personExist(name)) == null)
-			throw new PersonNotFoundException(name);
+		Client authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
 
 		authenticatedClient.getPersons().remove(person);
 		personServiceDb.delete(person);
@@ -120,15 +98,8 @@ public class PersonController {
 	@GetMapping("/verify")
 	public ResponseEntity<?> verify(@RequestBody Person newPerson)
 	{
-		Client authenticatedClient;
-		Person person;
+		Person person = getValidPersonByName(newPerson.getName());
 		HashMap<String, Object> body = new HashMap<>();
-
-		authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
-		if (authenticatedClient == null)
-			throw new ClientNotAuthenticatedException("anonymous");
-		if ((person = authenticatedClient.personExist(newPerson.getName())) == null)
-			throw new PersonNotFoundException(newPerson.getName());
 
 		Double score1 = 100
 				* Math.min(person.getTotalElapsedTime(), newPerson.getTotalElapsedTime())
@@ -137,10 +108,33 @@ public class PersonController {
 				* Math.min(person.getTotalPressTime(), newPerson.getTotalPressTime())
 				/ Math.max(person.getTotalPressTime(), newPerson.getTotalPressTime());
 		body.put("status", HttpStatus.OK);
+		body.put("action", "verified");
 		body.put("person", person);
 		body.put("score_tet", score1.intValue());
 		body.put("score_tpt", score2.intValue());
 
 		return new ResponseEntity<>(body, HttpStatus.OK);
+	}
+
+	private Person getValidPersonByName(String name)
+	{
+		Client authenticatedClient;
+		Person person;
+		HashMap<String, Object> body = new HashMap<>();
+
+		authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
+		if (authenticatedClient == null)
+			throw new ClientNotAuthenticatedException("anonymous");
+		if ((person = authenticatedClient.personExist(name)) == null)
+			throw new PersonNotFoundException(name);
+		return person;
+	}
+
+	private Client getValidAuthenticatedClient()
+	{
+		Client authenticatedClient = clientSecurityUtil.getAuthenticatedClient();
+		if (authenticatedClient == null)
+			throw new ClientNotAuthenticatedException("anonymous");
+		return authenticatedClient;
 	}
 }
